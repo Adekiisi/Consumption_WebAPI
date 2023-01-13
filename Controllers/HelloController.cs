facea511c9ffce6d20cc6d38d3070e4343f578f8
Iteoluwakiisi Adewole <oluwaiteadewole@gmail.com>
2023-01-13 03:35:50 +0100

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreationWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        public string[] myData = { "Data1", "Data2", "Data3" };
+        [HttpGet]
+        public string[] getData()
+        {
+            return myData;
+        }
+    }
+}
